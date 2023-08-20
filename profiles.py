f18a530f79bf64cd5e3c7b3e36cac56a2d8bd890
Prashant Marathay <pmarathay@infiniteoptions.com>
2023-08-19 22:52:30 -0700

    modified profiles.py

--- profiles.py
+++ profiles.py
@@ -3,7 +3,8 @@ from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from data import connect, disconnect, execute, helper_upload_img, helper_icon_img
+# from data import connect, disconnect, execute, helper_upload_img, helper_icon_img
+from data_pm import connect, uploadImage, s3
 import boto3
 import json
 from datetime import date, datetime, timedelta
@@ -23,28 +24,23 @@ class OwnerProfile(Resource):
     def get(self, owner_id):
         print('in Owner Profile')
         response = {}
-        conn = connect()
 
-        try:
-            profileQuery = (""" 
+        with connect() as db:
+            print("in connect loop")
+            profileQuery = db.execute(""" 
                     -- OWNER PROFILE
                     SELECT * FROM space.ownerProfileInfo
                     WHERE owner_uid = \'""" + owner_id + """\';
                     """)
             
 
-            print("Query: ", profileQuery)
-            items = execute(profileQuery, "get", conn)
-            print(items)
-            response["Profile"] = items["result"]
+            # print("Query: ", profileQuery)
+            # items = execute(profileQuery, "get", conn)
+            # print(items)
+            response["Profile"] = profileQuery 
 
 
             return response
-
-        except:
-            print("Error in Profile Query")
-        finally:
-            disconnect(conn)
 
 
 class TenantProfile(Resource):
@@ -53,25 +49,18 @@ class TenantProfile(Resource):
     def get(self, tenant_id):
         print('in Tenant Profile')
         response = {}
-        conn = connect()
 
-        try:
-            profileQuery = (""" 
+        with connect() as db:
+            print("in connect loop")
+            profileQuery = db.execute(""" 
                     -- TENANT PROFILE
                     SELECT * FROM space.tenantProfileInfo
                     WHERE tenant_uid = \'""" + tenant_id + """\';
                     """)
             
 
-            print("Query: ", profileQuery)
-            items = execute(profileQuery, "get", conn)
-            print(items)
-            response["Profile"] = items["result"]
-
-
+            # print("Query: ", profileQuery)
+            # items = execute(profileQuery, "get", conn)
+            # print(items)
+            response["Profile"] = profileQuery
             return response
-
-        except:
-            print("Error in Profile Query")
-        finally:
-            disconnect(conn)
