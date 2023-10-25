5e04f8d3105b5c229ceeede7c8e3ab4527ffe9b3
Prashant Marathay <pmarathay@infiniteoptions.com>
2023-10-25 13:44:47 -0700

    updated Lease endpoints

--- leases.py
+++ leases.py
@@ -113,6 +113,40 @@ class LeaseDetails(Resource):
 
 
 class LeaseApplication(Resource):
+
+    def __call__(self):
+        print("In Lease Application")
+
+# CHECK IF TENANT HAS APPLIED TO THIS PROPERTY BEFORE
+    def get(self, tenant_id, property_id):
+        print("In Lease Application GET")
+        print(tenant_id, property_id)
+        response = {}
+
+        with connect() as db:
+            # print("in get lease applications")
+            leaseQuery = db.execute(""" 
+                    -- FIND LEASE-TENANT APPLICATION
+                    SELECT *
+                    FROM space.lease_tenant
+                    LEFT JOIN space.leases ON lt_lease_id = lease_uid
+                    WHERE lt_tenant_id = \'""" + tenant_id + """\' AND lease_property_id = \'""" + property_id + """\';
+                    """)
+            print(leaseQuery)
+            print(leaseQuery['code'])
+            print(len(leaseQuery['result']))
+            
+
+            if leaseQuery['code'] == 200 and int(len(leaseQuery['result']) > 0):
+                # return("Tenant has already applied")
+                print(leaseQuery['result'][0]['lt_lease_id'])
+                return(leaseQuery['result'][0]['lt_lease_id'])
+
+        return ("New Application")
+
+
+
+
     # decorators = [jwt_required()]
 
     # Trigger to generate UIDs created using Alter Table in MySQL
@@ -124,9 +158,77 @@ class LeaseApplication(Resource):
         data = request.get_json(force=True)
         print(data)
 
-        with connect() as db:
-            response = db.insert('leases',data)
-            return response
+        # Remove tenenat_uid from data and store as an independant variable 
+        if 'tenant_uid' in data:
+            print(data['tenant_uid'])
+            tenant_uid = data.get('tenant_uid')
+            print(tenant_uid)
+            del data['tenant_uid']
+
+
+        ApplicationStatus = LeaseApplication.get(self, tenant_uid, data.get('lease_property_id'))
+        print(ApplicationStatus)
+        if ApplicationStatus != "New Application":
+            print(ApplicationStatus)
+
+            # key = {'lease_uid': data.pop('lease_uid')}
+            # key = ApplicationStatus
+            # print(key)
+            key = {'lease_uid': ApplicationStatus}
+            print(key)
+        
+            with connect() as db:
+                response = db.update('leases', key, data)
+
+            print(response)
+
+        else:
+            ApplicationStatus = "New"
+            print(ApplicationStatus)
+
+            with connect() as db:
+                response = db.insert('leases',data)
+
+
+            with connect() as db:
+                print("Need to find lease_uid")
+                leaseQuery = db.execute(""" 
+                        -- FIND lease_uid
+                        SELECT * FROM space.leases
+                        ORDER BY lease_uid DESC
+                        LIMIT 1;
+                        """)
+            
+            print(leaseQuery)
+            print(leaseQuery['result'][0]['lease_uid'])
+            lease_id = leaseQuery['result'][0]['lease_uid']
+
+            tenant_responsibiity = str(1)
+            print(tenant_responsibiity, type(tenant_responsibiity))
+
+            print(lease_id, tenant_uid, tenant_responsibiity )
+
+            with connect() as db:
+                print("Add record in lease_tenant table")
+                ltQuery = (""" 
+                        INSERT INTO space.lease_tenant
+                        SET lt_lease_id = \'""" + lease_id + """\'
+                           , lt_tenant_id = \'""" + tenant_uid + """\'
+                           , lt_responsibility = \'""" + tenant_responsibiity + """\';
+                        """)
+                
+                response = db.execute(ltQuery, [], 'post')
+            
+                print(ltQuery)
+
+
+        # key = {'lease_uid': data.pop('lease_uid')}
+        # print(key)
+
+        
+        return response
+        
+
         
 
     def put(self):
