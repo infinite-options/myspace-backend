f33df5d73acf2c58277c189b9e75724dadd4e124
Prashant Marathay <pmarathay@infiniteoptions.com>
2023-10-26 01:10:33 -0700

    rewrote LeaseDetails endpoint

--- leases.py
+++ leases.py
@@ -25,80 +25,48 @@ class LeaseDetails(Resource):
             if filter_id[:3] == "110":
 
                 leaseQuery = db.execute(""" 
-                        -- PROPERTY MANAGEMENT AND OWNER LEASES
-                        SELECT -- *,
-                            lease_uid, lease_property_id, lease_start, lease_end, lease_status, lease_early_end_date, lease_renew_status, move_out_date
-                            , property_owner_id, po_owner_percent, owner_first_name, owner_last_name, owner_phone_number, owner_email, owner_address, owner_unit, owner_city, owner_state, owner_zip
-                            , property_address, property_unit, property_city, property_state, property_zip, property_type
-                            , leaseFees_uid, fee_name, fee_type, charge, due_by, late_by, late_fee, perDay_late_fee, frequency, available_topay
-                            , ld_created_date, ld_type, ld_name, ld_description, ld_shared, ld_link
-                            , lt_tenant_id, lt_responsibility, tenant_first_name, tenant_last_name, tenant_email, tenant_phone_number
-                            , contract_uid, contract_status, business_uid
+                        -- OWNER LEASES
+                        SELECT *
                         FROM space.leases
-                        LEFT JOIN space.o_details ON property_id = lease_property_id
                         LEFT JOIN space.properties ON property_uid = lease_property_id
-                        LEFT JOIN space.leaseFees ON lease_uid = fees_lease_id
-                        LEFT JOIN space.leaseDocuments ON lease_uid = ld_lease_id
+                        LEFT JOIN space.o_details ON property_id = lease_property_id
                         LEFT JOIN space.t_details ON lease_uid = lt_lease_id
                         LEFT JOIN space.b_details ON contract_property_id = lease_property_id
-                        WHERE lease_status = "ACTIVE"
-                            -- AND contract_status = "ACTIVE"
-                            AND fee_name = "RENT"
-                            AND ld_type = "LEASE"
-                            AND property_owner_id = \'""" + filter_id + """\';
+                        WHERE owner_uid = \'""" + filter_id + """\'
+                            AND (lease_status = "NEW" OR lease_status = "REJECTED" OR lease_status = "PROCESSING" OR lease_status = "ACTIVE" OR lease_status = "REFUSED")
+
                         """)
                 
             elif filter_id[:3] == "350":
 
                 leaseQuery = db.execute(""" 
-                        -- PROPERTY MANAGEMENT AND OWNER LEASES
-                        SELECT -- *,
-                            lease_uid, lease_property_id, lease_start, lease_end, lease_status, lease_early_end_date, lease_renew_status, move_out_date
-                            , property_owner_id, po_owner_percent, owner_first_name, owner_last_name, owner_phone_number, owner_email, owner_address, owner_unit, owner_city, owner_state, owner_zip
-                            , property_address, property_unit, property_city, property_state, property_zip, property_type
-                            , leaseFees_uid, fee_name, fee_type, charge, due_by, late_by, late_fee, perDay_late_fee, frequency, available_topay
-                            , ld_created_date, ld_type, ld_name, ld_description, ld_shared, ld_link
-                            , lt_tenant_id, lt_responsibility, tenant_first_name, tenant_last_name, tenant_email, tenant_phone_number
-                            , contract_uid, contract_status, business_uid
+                        -- TENANT LEASES
+                        SELECT *
                         FROM space.leases
-                        LEFT JOIN space.o_details ON property_id = lease_property_id
                         LEFT JOIN space.properties ON property_uid = lease_property_id
-                        LEFT JOIN space.leaseFees ON lease_uid = fees_lease_id
-                        LEFT JOIN space.leaseDocuments ON lease_uid = ld_lease_id
+                        LEFT JOIN space.o_details ON property_id = lease_property_id
                         LEFT JOIN space.t_details ON lease_uid = lt_lease_id
                         LEFT JOIN space.b_details ON contract_property_id = lease_property_id
-                        WHERE lease_status = "ACTIVE"
-                            AND contract_status = "ACTIVE"
-                            AND fee_name = "RENT"
-                            AND ld_type = "LEASE"
-                            AND lt_tenant_id = \'""" + filter_id + """\';
+                        WHERE lt_tenant_id = \'""" + filter_id + """\'
+                            AND (lease_status = "NEW" OR lease_status = "REJECTED" OR lease_status = "PROCESSING" OR lease_status = "ACTIVE" OR lease_status = "REFUSED")
                         """)
 
-            else:
+            elif filter_id[:3] == "600":
 
                 leaseQuery = db.execute(""" 
-                        -- PROPERTY MANAGEMENT AND OWNER LEASES
-                        SELECT -- *,
-                            lease_uid, lease_property_id, lease_start, lease_end, lease_status, lease_early_end_date, lease_renew_status, move_out_date
-                            , property_owner_id, po_owner_percent, owner_first_name, owner_last_name, owner_phone_number, owner_email, owner_address, owner_unit, owner_city, owner_state, owner_zip
-                            , property_address, property_unit, property_city, property_state, property_zip, property_type
-                            , leaseFees_uid, fee_name, fee_type, charge, due_by, late_by, late_fee, perDay_late_fee, frequency, available_topay
-                            , ld_created_date, ld_type, ld_name, ld_description, ld_shared, ld_link
-                            , lt_tenant_id, lt_responsibility, tenant_first_name, tenant_last_name, tenant_email, tenant_phone_number
-                            , contract_uid, contract_status, business_uid
+                        -- PROPERTY MANAGEMENT LEASES
+                        SELECT *
                         FROM space.leases
-                        LEFT JOIN space.o_details ON property_id = lease_property_id
                         LEFT JOIN space.properties ON property_uid = lease_property_id
-                        LEFT JOIN space.leaseFees ON lease_uid = fees_lease_id
-                        LEFT JOIN space.leaseDocuments ON lease_uid = ld_lease_id
+                        LEFT JOIN space.o_details ON property_id = lease_property_id
                         LEFT JOIN space.t_details ON lease_uid = lt_lease_id
                         LEFT JOIN space.b_details ON contract_property_id = lease_property_id
-                        WHERE lease_status = "ACTIVE"
-                            AND contract_status = "ACTIVE"
-                            AND fee_name = "RENT"
-                            AND ld_type = "LEASE"
-                            AND business_uid = \'""" + filter_id + """\';
+                        WHERE contract_business_id = \'""" + filter_id + """\'
+                            AND (lease_status = "NEW" OR lease_status = "REJECTED" OR lease_status = "PROCESSING" OR lease_status = "ACTIVE" OR lease_status = "REFUSED")
                         """)
+                
+            else:
+                leaseQuery = "UID Not Found"
 
             # print("Lease Query: ", leaseQuery)
             # items = execute(leaseQuery, "get", conn)
