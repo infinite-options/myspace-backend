da870684c621a52ae940d225a38c7e98b0e58e26
Vishal968 <vishalphaugat968@gmail.com>
2023-10-27 12:54:27 -0700

    modified contracts endpoint

--- contracts.py
+++ contracts.py
@@ -6,22 +6,85 @@ from data_pm import connect
 
 
 class Contracts(Resource):
+    # def post(self):
+    #     print('in Contracts')
+    #     payload = request.get_json()
+    #     with connect() as db:
+    #         response = db.insert('contracts', payload)
+    #     return response
+
     def post(self):
-        print('in Contracts')
-        payload = request.get_json()
+        print("In Contracts")
         with connect() as db:
-            response = db.insert('contracts', payload)
+            data = request.form
+            fields = [
+                 "contract_property_id"
+                , 'contract_business_id'
+                , "contract_start_date"
+                , 'contract_end_date'
+                , "contract_fees"
+                , "contract_assigned_contacts"
+                , "contract_documents"
+                , "contract_name"
+                , "contract_status"
+                , "contract_early_end_date"
+            ]
+            newContract = {}
+            for field in fields:
+                newContract[field] = data.get(field)
+
+            response = db.insert('contracts', newContract)
         return response
 
     def put(self):
-        print('in Contracts')
-        payload = request.get_json()
-        if payload.get('contract_uid') is None:
-            raise BadRequest("Request failed, no UID in payload.")
-        key = {'contract_uid': payload.pop('contract_uid')}
+        response = {}
+        data = request.form
+        contract_id = data.get("contract_uid")
+        print(f"Updating contract with ID {contract_id}")
         with connect() as db:
-            response = db.update('contracts', key, payload)
+
+            fields = [
+                "contract_property_id",
+                "contract_business_id",
+                "contract_start_date",
+                "contract_end_date",
+                "contract_fees",
+                "contract_assigned_contacts",
+                "contract_documents",
+                "contract_name",
+                "contract_status",
+                "contract_early_end_date"
+            ]
+
+            updated_contract = {}
+            for field in fields:
+                if field in data:
+                    updated_contract[field] = data.get(field)
+
+            # Check if there are fields to update
+            if updated_contract:
+                # Update the contract in the database based on the contract_id
+                key = {'contract_uid': contract_id}
+                result = db.update('contracts', key, updated_contract)
+
+                if result:
+                    response["message"] = f"Contract with ID {contract_id} has been updated."
+                else:
+                    response["error"] = f"Contract with ID {contract_id} not found."
+            else:
+                response["error"] = "No fields to update."
+
         return response
+
+    # def put(self):
+    #     print('in Contracts')
+    #     payload = request.get_json()
+    #     if payload.get('contract_uid') is None:
+    #         raise BadRequest("Request failed, no UID in payload.")
+    #     key = {'contract_uid': payload.pop('contract_uid')}
+    #     with connect() as db:
+    #         response = db.update('contracts', key, payload)
+    #     return response
 
     def get(self, user_id):
         if user_id.startswith("600"):
