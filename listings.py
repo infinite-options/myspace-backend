85b850e0fcb6e1e375d6dcc752f097d154003cc9
Sahil Shrivastava <51322128+sahilalexshrivastava@users.noreply.github.com>
2023-11-30 12:05:38 -0800

    Update listings.py
    Listings updated

--- listings.py
+++ listings.py
@@ -13,7 +13,7 @@ class Template(Resource):
         return response
     
 class Listings (Resource):
-    def get(self):
+        def get(self, tenant_id):
         print('in Listings')
         response = {}
         # conn = connect()
@@ -24,10 +24,17 @@ class Listings (Resource):
                     -- AVAILABLE LISTINGS
                     SELECT * FROM space.contracts
                     LEFT JOIN space.properties ON contract_property_id = property_uid
-                    LEFT JOIN (SELECT * FROM space.leases WHERE lease_end < DATE_ADD(CURDATE(), INTERVAL 1 MONTH) AND lease_end != "") AS l ON lease_property_id = property_uid
-                    WHERE contract_status = "ACTIVE" AND property_available_to_rent = "1"
+                    LEFT JOIN (SELECT JSON_ARRAYAGG(JSON_OBJECT
+                                                    ('tenant_uid', tenant_uid,
+                                                    'lt_responsibility', lt_responsibility,
+                                                    'tenant_first_name', tenant_first_name,
+                                                    'tenant_last_name', tenant_last_name,
+                                                    'tenant_phone_number', tenant_phone_number,
+                                                    'tenant_email', tenant_email
+                                                    )) AS tenants, space.leases.*, lt_tenant_id, CASE WHEN lt_tenant_id = \'""" + tenant_id + """\' AND lease_status = "ACTIVE" THEN 1 ELSE NULL END AS is_target_tenant FROM space.leases LEFT JOIN space.t_details ON lt_lease_id = lease_uid WHERE (lease_end < DATE_ADD(CURDATE(), INTERVAL 1 MONTH) AND lease_end != "") OR (lt_tenant_id = \'""" + tenant_id + """\') GROUP BY lt_lease_id) AS l ON lease_property_id = property_uid
+                    WHERE (contract_status = "ACTIVE" AND property_available_to_rent = "1") OR (lt_tenant_id = \'""" + tenant_id + """\')
+                    GROUP BY contract_property_id;
                     """)
-            
 
             # print("Query: ", listingsQuery)
             # items = execute(istingsQuery, "get", conn)
