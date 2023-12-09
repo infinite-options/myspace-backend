62ed0e30f172be0d4398aec31b99cf83b6a57595
Prashant Marathay <pmarathay@infiniteoptions.com>
2023-12-09 01:57:12 -0700

    updated listings endpoint

--- listings.py
+++ listings.py
@@ -13,36 +13,50 @@ class Template(Resource):
         return response
     
 class Listings (Resource):
-    def get(self, tenant_id):
+    def get(self,tenant_id):
         print('in Listings')
         response = {}
         # conn = connect()
 
         with connect() as db:
-            print("in connect loop")
+            print("in listings loop")
             listingsQuery = db.execute(""" 
-                     -- AVAILABLE LISTINGS
-                    SELECT * FROM space.properties
-                    RIGHT JOIN (SELECT * FROM space.contracts WHERE contract_status = "ACTIVE") as c 
+                    -- AVAILABLE LISTINGS EXCLUDING ACTIVE LEASES EXPIRING MORE THAT 2 MONTH FROM NOW
+                    SELECT * 
+                    FROM (
+                        SELECT * FROM space.properties
+                        RIGHT JOIN (
+                            SELECT * FROM space.contracts 
+                            WHERE contract_status = "ACTIVE") AS c 
                         ON contract_property_id = property_uid
-                    LEFT JOIN (SELECT lease_property_id
-                            , JSON_ARRAYAGG(JSON_OBJECT
-                            ('lt_tenant_id', lt_tenant_id
-                            , 'lt_responsibility', lt_responsibility
-                            , 'lease_status', lease_status
-                            , 'lease_end', lease_end
-                            )) AS tenants
-                        FROM space.leases
-                        LEFT JOIN space.lease_tenant ON lt_lease_id = lease_uid
-                        WHERE lease_status NOT IN ("EXPIRED","TERMINATED") AND
-                            -- lt_tenant_id = "350-000084" 
-                            lt_tenant_id = \'""" + tenant_id + """\'
-                        GROUP BY lease_property_id) as l
-                        ON lease_property_id = property_uid
-                    WHERE property_available_to_rent = 1;
+                        WHERE property_available_to_rent = 1 ) AS p
+                    LEFT JOIN ( 
+                        SELECT * FROM space.leases 
+                        -- WHERE lease_status = "ACTIVE"
+                        WHERE lease_status = "ACTIVE" AND STR_TO_DATE(lease_end, '%m-%d-%Y') > DATE_ADD(CURDATE(), INTERVAL 2 MONTH)
+                        ) AS l
+                    ON lease_property_id = property_uid
+                    WHERE lease_status IS null;
                     """)
 
             # print("Query: ", listingsQuery)
             # items = execute(istingsQuery, "get", conn)
-            response["Property_Dashboard"] = listingsQuery
+            response["Available Listings"] = listingsQuery
+
+        with connect() as db:
+            print("in tenant loop")
+            tenantsQuery = db.execute(""" 
+
+                    -- SHOW TENANT LEASES
+                    SELECT * FROM space.lease_tenant
+                    LEFT JOIN space.leases ON lease_uid = lt_lease_id
+                    -- WHERE lt_tenant_id = "350-000084"
+                    WHERE lt_tenant_id = \'""" + tenant_id + """\'
+                    """)
+            # print("Query: ", tenantsQuery)
+            # items = execute(istingsQuery, "get", conn)
+            response["Tenant Leases"] = tenantsQuery
+
             return response
+                                       
+                                                   
