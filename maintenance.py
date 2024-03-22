91b6aa8d013df135686feeb0990fa3fa1c6a075c
pmarathay <pmarathay@infiniteoptions.com>
2024-03-22 10:26:40 -0700

    removed commented code and commented print statements

--- maintenance.py
+++ maintenance.py
@@ -298,33 +298,17 @@ class MaintenanceRequests(Resource):
         print('in maintenanceRequests PUT')
         response = {}
         payload = request.form
-        print(payload)
+        # print(payload)
         if payload.get('maintenance_request_uid') is None:
             raise BadRequest("Request failed, no UID in payload.")
         key = {'maintenance_request_uid': payload['maintenance_request_uid']}
-        print("Key: ", key)
+        # print("Key: ", key)
         maintenanceRequests = {k: v for k, v in payload.items()}
-        print(maintenanceRequests)
+        # print(maintenanceRequests)
         with connect() as db:
             print(key, payload)
             response = db.update('maintenanceRequests', key, maintenanceRequests)
         return response
-    
-    # def put(self):
-    #     print('in bills')
-
-    #     payload = request.form
-        
-    #     if payload.get('maintenance_request_uid') is None:
-    #         raise BadRequest("Request failed, no UID in payload.")
-    #     key = {'maintenance_request_uid': payload['maintenance_request_uid']}
-    #     print("Key: ", key)
-    #     bills = {k: v for k, v in payload.items()}
-    #     print("KV Pairs: ", bills)
-    #     with connect() as db:
-    #         print("In actual PUT")
-    #         response = db.update('maintenanceRequests', key, bills)
-    #     return response
 
 
 class MaintenanceQuotes(Resource):
