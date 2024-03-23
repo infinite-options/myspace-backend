b7a52d13776a39cb38daba551761c8d4be27a911
rohitkphilip <rohitkurian.philip@sjsu.edu>
2024-03-22 21:34:48 -0700

    s3 credentials fix - do not merge

--- data_pm.py
+++ data_pm.py
@@ -6,8 +6,19 @@ from botocore.response import StreamingBody
 from decimal import Decimal
 from werkzeug.datastructures import FileStorage
 import mimetypes
+import os
 
-s3 = boto3.client('s3')
+# s3 = boto3.client('s3')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+print("ROHIT - AWS_ACCESS_KEY_ID - ", AWS_ACCESS_KEY_ID)
+print("ROHIT - AWS_SECRET_ACCESS_KEY - ",AWS_SECRET_ACCESS_KEY)
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID, 
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+
 
 
 # def uploadImage(file, key, content):
