64b9345e36f5bcf556f243900f327890b4724ef8
pmarathay <pmarathay@infiniteoptions.com>
2024-05-29 11:06:45 -0700

    updated cashflow endpoints

--- cashflow.py
+++ cashflow.py
@@ -891,6 +891,7 @@ class Cashflow(Resource):
                             , property_address, property_unit
                         FROM space.pp_details
                         WHERE receiver_profile_uid = \'""" + user_id + """\'
+                        -- WHERE receiver_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'REVENUE' OR pur_cf_type = 'revenue')
@@ -912,6 +913,7 @@ class Cashflow(Resource):
                             , property_address, property_unit
                         FROM space.pp_details
                         WHERE receiver_profile_uid = \'""" + user_id + """\'
+                        -- WHERE receiver_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'REVENUE' OR pur_cf_type = 'revenue')
@@ -933,6 +935,7 @@ class Cashflow(Resource):
                             , property_address, property_unit
                         FROM space.pp_details
                         WHERE receiver_profile_uid = \'""" + user_id + """\'
+                        -- WHERE receiver_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'REVENUE' OR pur_cf_type = 'revenue')
@@ -952,10 +955,11 @@ class Cashflow(Resource):
                             , pur_cf_type, purchase_type
                             , pur_amount_due, pur_notes, pur_description, total_paid, amt_remaining, payment_status
                             , property_address, property_unit
-                , space.bills.*
+                            , space.bills.*
                         FROM space.pp_details
-                LEFT JOIN space.bills ON pur_bill_id = bill_uid AND pur_bill_id IS NOT NULL
+                        LEFT JOIN space.bills ON pur_bill_id = bill_uid AND pur_bill_id IS NOT NULL
                         WHERE receiver_profile_uid = \'""" + user_id + """\'
+                        -- WHERE receiver_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'REVENUE' OR pur_cf_type = 'revenue')
@@ -969,14 +973,15 @@ class Cashflow(Resource):
 
                     # # EXPENSES
                     response_expense_by_year = db.execute("""
-                        -- ALL REVENUE TRANSACTIONS AFFECTING A PARTICULAR OWNER BY PROPERTY BY YEAR
+                        -- ALL EXPENSE TRANSACTIONS AFFECTING A PARTICULAR OWNER BY PROPERTY BY YEAR
                         SELECT -- * , 
                             cf_year -- , cf_month, pur_due_date
                             , pur_cf_type -- , purchase_type
                             , sum(pur_amount_due), sum(total_paid), sum(amt_remaining) -- , payment_status
                             , property_address, property_unit
                         FROM space.pp_details
-                        WHERE pur_payer = \'""" + user_id + """\'
+                        WHERE payer_profile_uid = \'""" + user_id + """\'
+                        -- WHERE payer_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'EXPENSE' OR pur_cf_type = 'expense')
@@ -990,14 +995,15 @@ class Cashflow(Resource):
                     # print("In Cashflow TTM 5")
 
                     response_expense_by_month = db.execute("""
-                        -- ALL REVENUE TRANSACTIONS AFFECTING A PARTICULAR OWNER BY PROPERTY BY MONTH
+                        -- ALL EXPENSE TRANSACTIONS AFFECTING A PARTICULAR OWNER BY PROPERTY BY MONTH
                         SELECT -- * , 
                             cf_year, cf_month -- , pur_due_date
                             , pur_cf_type -- , purchase_type
                             , sum(pur_amount_due), sum(total_paid), sum(amt_remaining) -- , payment_status
                             , property_address, property_unit
                         FROM space.pp_details
-                        WHERE pur_payer = \'""" + user_id + """\'
+                        WHERE payer_profile_uid = \'""" + user_id + """\'
+                        -- WHERE payer_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'EXPENSE' OR pur_cf_type = 'expense')
@@ -1011,14 +1017,15 @@ class Cashflow(Resource):
                     # print("In Cashflow TTM 6")
 
                     response_expense_by_month_by_type = db.execute("""
-                        -- ALL REVENUE TRANSACTIONS AFFECTING A PARTICULAR OWNER BY PROPERTY BY MONTH BY PURCHASE TYPE
+                        -- ALL EXPENSE TRANSACTIONS AFFECTING A PARTICULAR OWNER BY PROPERTY BY MONTH BY PURCHASE TYPE
                         SELECT -- * , 
                             cf_year, cf_month -- , pur_due_date
                             , pur_cf_type, purchase_type
                             , sum(pur_amount_due), sum(total_paid), sum(amt_remaining) -- , payment_status
                             , property_address, property_unit
                         FROM space.pp_details
-                        WHERE pur_payer = \'""" + user_id + """\'
+                        WHERE payer_profile_uid = \'""" + user_id + """\'
+                        -- WHERE payer_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'EXPENSE' OR pur_cf_type = 'expense')
@@ -1038,10 +1045,11 @@ class Cashflow(Resource):
                             , pur_cf_type, purchase_type
                             , pur_amount_due, pur_notes, pur_description, total_paid, amt_remaining, payment_status
                             , property_address, property_unit
-                , space.bills.*
+                            , space.bills.*
                         FROM space.pp_details
-                LEFT JOIN space.bills ON pur_bill_id = bill_uid AND pur_bill_id IS NOT NULL
-                        WHERE pur_payer = \'""" + user_id + """\'
+                        LEFT JOIN space.bills ON pur_bill_id = bill_uid AND pur_bill_id IS NOT NULL
+                        WHERE payer_profile_uid = \'""" + user_id + """\'
+                        -- WHERE payer_profile_uid = '110-000003'
                             AND STR_TO_DATE(pur_due_date, '%m-%d-%Y') > DATE_SUB(NOW(), INTERVAL 365 DAY)
                             AND purchase_status != 'DELETED'
                             AND (pur_cf_type = 'EXPENSE' OR pur_cf_type = 'expense')
@@ -1082,51 +1090,50 @@ class HappinessMatrix(Resource):
             print("in connect loop")
 
             vacancy = db.execute(""" 
-SELECT 
-    property_owner_id as owner_uid,
-    COUNT(CASE WHEN rent_status = 'VACANT' THEN 1 END) as vacancy_num, 
-    COUNT(*) AS total_properties,
-    cast(COUNT(CASE WHEN rent_status = 'VACANT' THEN 1 END)*-100/COUNT(*) as decimal) as vacancy_perc
-FROM (
-    SELECT *,
-        CASE
-            WHEN (lease_status = 'ACTIVE' AND payment_status IS NOT NULL) THEN payment_status
-            WHEN (lease_status = 'ACTIVE' AND payment_status IS NULL) THEN 'UNPAID'
-            ELSE 'VACANT'
-        END AS rent_status
-    FROM (
-        SELECT *
-        FROM space.property_owner
-        LEFT JOIN space.properties ON property_uid = property_id
-        LEFT JOIN (SELECT * FROM space.leases WHERE lease_status = 'ACTIVE') AS l ON property_uid = lease_property_id
-        LEFT JOIN (SELECT * FROM space.contracts WHERE contract_status = 'ACTIVE') AS c ON contract_property_id = property_uid
-        WHERE contract_business_id = \'""" + user_id + """\'
-    ) AS o
-    LEFT JOIN (
-        SELECT *
-        FROM space.pp_status 
-        WHERE (purchase_type = 'RENT' OR ISNULL(purchase_type))
-            AND (cf_month = DATE_FORMAT(NOW(), '%M') OR ISNULL(cf_month))
-            AND (cf_year = DATE_FORMAT(NOW(), '%Y') OR ISNULL(cf_year))
-    ) as r
-    ON pur_property_id = property_id
-) AS rs
-GROUP BY property_owner_id;
+                        SELECT 
+                            property_owner_id as owner_uid,
+                            COUNT(CASE WHEN rent_status = 'VACANT' THEN 1 END) as vacancy_num, 
+                            COUNT(*) AS total_properties,
+                            cast(COUNT(CASE WHEN rent_status = 'VACANT' THEN 1 END)*-100/COUNT(*) as decimal) as vacancy_perc
+                        FROM (
+                            SELECT *,
+                                CASE
+                                    WHEN (lease_status = 'ACTIVE' AND payment_status IS NOT NULL) THEN payment_status
+                                    WHEN (lease_status = 'ACTIVE' AND payment_status IS NULL) THEN 'UNPAID'
+                                    ELSE 'VACANT'
+                                END AS rent_status
+                            FROM (
+                                SELECT *
+                                FROM space.property_owner
+                                LEFT JOIN space.properties ON property_uid = property_id
+                                LEFT JOIN (SELECT * FROM space.leases WHERE lease_status = 'ACTIVE') AS l ON property_uid = lease_property_id
+                                LEFT JOIN (SELECT * FROM space.contracts WHERE contract_status = 'ACTIVE') AS c ON contract_property_id = property_uid
+                                WHERE contract_business_id = \'""" + user_id + """\'
+                            ) AS o
+                            LEFT JOIN (
+                                SELECT *
+                                FROM space.pp_status 
+                                WHERE (purchase_type = 'RENT' OR ISNULL(purchase_type))
+                                    AND (cf_month = DATE_FORMAT(NOW(), '%M') OR ISNULL(cf_month))
+                                    AND (cf_year = DATE_FORMAT(NOW(), '%Y') OR ISNULL(cf_year))
+                            ) as r
+                            ON pur_property_id = property_id
+                        ) AS rs
+                        GROUP BY property_owner_id;
                         """)
             response["vacancy"] = vacancy
             for i in range(0,len(response["vacancy"]["result"])):
                 response["vacancy"]["result"][i]["vacancy_perc"] = float(response["vacancy"]["result"][i]["vacancy_perc"])
             delta_cashflow = db.execute("""
-
-SELECT -- * , 
-space.p_details.owner_uid AS owner_id,space.p_details.owner_first_name,space.p_details.owner_last_name,space.p_details.owner_photo_url,
-cast(ifnull(-100*ABS((ifnull(sum(pur_amount_due),0)-ifnull(sum(total_paid),0))/ifnull(sum(pur_amount_due),0)), 0) as decimal(10,2)) as delta_cashflow_perc 
-, cast(ifnull(sum(total_paid),0) as decimal(10.2)) as cashflow , cast(ifnull(sum(pur_amount_due),0) as decimal(10,2)) as expected_cashflow -- , payment_status
-FROM space.p_details
-LEFT JOIN space.pp_details ON space.p_details.owner_uid = space.pp_details.pur_payer
-WHERE space.p_details.contract_business_id = \'""" + user_id + """\'
-GROUP BY space.p_details.owner_uid;
-	            """)
+                        SELECT -- * , 
+                        space.p_details.owner_uid AS owner_id,space.p_details.owner_first_name,space.p_details.owner_last_name,space.p_details.owner_photo_url,
+                        cast(ifnull(-100*ABS((ifnull(sum(pur_amount_due),0)-ifnull(sum(total_paid),0))/ifnull(sum(pur_amount_due),0)), 0) as decimal(10,2)) as delta_cashflow_perc 
+                        , cast(ifnull(sum(total_paid),0) as decimal(10.2)) as cashflow , cast(ifnull(sum(pur_amount_due),0) as decimal(10,2)) as expected_cashflow -- , payment_status
+                        FROM space.p_details
+                        LEFT JOIN space.pp_details ON space.p_details.owner_uid = space.pp_details.pur_payer
+                        WHERE space.p_details.contract_business_id = \'""" + user_id + """\'
+                        GROUP BY space.p_details.owner_uid;
+                        """)
 
             response["delta_cashflow"] = delta_cashflow
 
