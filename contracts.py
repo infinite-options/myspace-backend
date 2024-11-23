d6d6834cb0a3b90b9f0965d1be21b63b854cd899
pmarathay <pmarathay@infiniteoptions.com>
2024-11-23 11:37:44 -0800

    modified contracts query to accomodate multiple owners

--- contracts.py
+++ contracts.py
@@ -31,142 +31,6 @@ class Contracts(Resource):
         response = ContractDetails(user_id)
         return response
 
-        # if user_id.startswith("600"):
-        #     print('in ContractsByBusiness')
-        #     with connect() as db:
-        #         response = db.execute("""
-        #         SELECT -- *,
-        #             -- property_id, property_unit, property_address, property_city, property_state, property_zip, property_owner_id, po_owner_percent
-        #             p.*
-        #             , owner_uid, owner_user_id, owner_first_name, owner_last_name, owner_phone_number, owner_email
-        #             -- , owner_address, owner_unit, owner_city, owner_state, owner_zip
-        #             , owner_photo_url
-        #             , contract_uid, contract_property_id, contract_business_id, contract_start_date, contract_end_date, contract_fees, contract_assigned_contacts, contract_documents, contract_name, contract_status, contract_early_end_date, contract_end_notice_period, contract_m2m
-        #             , business_uid, business_user_id, business_type, business_name, business_phone_number, business_email
-        #             -- , business_address, business_unit, business_city, business_state, business_zip, business_photo_url
-        #         FROM space.o_details o
-        #         LEFT JOIN space.properties p ON o.property_id =p.property_uid 
-        #         LEFT JOIN space.b_details b ON o.property_id = b.contract_property_id
-        #         -- WHERE b.business_uid = '600-000011'
-        #         -- WHERE o.owner_uid = \'""" + user_id + """\';
-        #         WHERE b.business_uid = \'""" + user_id + """\';
-        #         """)
-        #         return response
-
-        # elif user_id.startswith("110"):
-        #     print('in ContractsByOwner')
-        #     with connect() as db:
-        #         # response = db.execute("""
-        #         # SELECT -- *,
-        #         #     property_id, property_owner_id, po_owner_percent
-        #         #     , owner_uid, owner_user_id, owner_first_name, owner_last_name, owner_phone_number, owner_email
-        #         #     -- , owner_address, owner_unit, owner_city, owner_state, owner_zip
-        #         #     , owner_photo_url
-        #         #     , contract_uid, contract_property_id, contract_business_id, contract_start_date, contract_end_date, contract_fees, contract_assigned_contacts, contract_documents, contract_name, contract_status, contract_early_end_date, contract_end_notice_period, contract_m2m
-        #         #     , business_uid, business_user_id, business_type, business_name, business_phone_number, business_email
-        #         #     -- , business_address, business_unit, business_city, business_state, business_zip, business_photo_url
-        #         #     , business_services_fees, business_locations
-        #         # FROM space.o_details o
-        #         # LEFT JOIN space.b_details b ON o.property_id = b.contract_property_id
-        #         # WHERE o.owner_uid = \'""" + user_id + """\';
-        #         # -- WHERE b.business_uid = \'""" + user_id + """\';
-        #         # """)
-        #         # return response
-
-        #         response = db.execute("""
-        #         SELECT -- *,
-        #             -- property_id, property_unit, property_address, property_city, property_state, property_zip, property_owner_id, po_owner_percent
-        #             p.*
-        #             , owner_uid, owner_user_id, owner_first_name, owner_last_name, owner_phone_number, owner_email
-        #             -- , owner_address, owner_unit, owner_city, owner_state, owner_zip
-        #             , owner_photo_url
-        #             , contract_uid, contract_property_id, contract_business_id, contract_start_date, contract_end_date, contract_fees, contract_assigned_contacts, contract_documents, contract_name, contract_status, contract_early_end_date, contract_end_notice_period, contract_m2m
-        #             , business_uid, business_user_id, business_type, business_name, business_phone_number, business_email, business_services_fees
-        #             -- , business_address, business_unit, business_city, business_state, business_zip, business_photo_url
-        #         FROM space.o_details o
-        #         LEFT JOIN space.properties p ON o.property_id =p.property_uid 
-        #         LEFT JOIN space.b_details b ON o.property_id = b.contract_property_id
-        #         -- WHERE b.business_uid = '600-000011'
-        #         -- WHERE o.owner_uid = '110-000003'
-        #         WHERE o.owner_uid = \'""" + user_id + """\';
-        #         -- WHERE b.business_uid = \'""" + user_id + """\';
-        #         """)
-        #         return response
-        # else:
-        #     return "No records for this Uid"
-
-    # def post(self):
-    #     print("In Contracts Start")
-    #     with connect() as db:
-    #         data = request.form
-    #         fields = [
-    #             'contract_business_id'
-    #             , "contract_start_date"
-    #             , 'contract_end_date'
-    #             , "contract_fees"
-    #             , "contract_assigned_contacts"
-    #             , "contract_documents"
-    #             , "contract_name"
-    #             , "contract_status"
-    #             , "contract_early_end_date"
-    #             , "contract_end_notice_period"
-    #             , "contract_m2m"
-    #         ]
-
-    #         # properties_l is a list since you may create contracts for multiple properties at once
-    #         # If contracts is called as part of AddProperty.jsx then it is a list of one property
-    #         properties_l = data.get("contract_property_ids")
-    #         # print("Properties affected: ", properties_l)
-    #         properties = json.loads(properties_l)
-    #         print("Properties affected: ", properties)
-    #         business_id = data.get('contract_business_id')
-    #         print(business_id)
-
-    #         # Check if the property already has any active renewals for the given business
-    #         index_to_remove = []
-    #         properties_removed = []
-    #         for i in range(len(properties)):
-    #             print('in Contracts Loop', len(properties), i, properties[i])
-    #             with connect() as db:
-    #                 response = db.execute("""
-    #                             SELECT * From space.contracts
-    #                             WHERE contract_property_id = \'""" + properties[i] + """\' AND contract_business_id = \'""" + business_id + """\'
-    #                             AND contract_status in ('NEW','SENT');
-    #                             """)
-    #                 if len(response['result']) != 0:
-    #                     index_to_remove.append(i)
-    #                     properties_removed.append(properties[i])
-    #         print("Response: ", response)
-    #         print("Properties ", properties)
-    #         print("Index to Remove: ", index_to_remove)
-    #         print("Properties to be removed: ", properties_removed)
-
-    #         # removes any properties from the List that already have contracts
-    #         for i in sorted(index_to_remove, reverse = True):
-    #             del properties[i]
-    #         print("Actual list of properties to be added:" , properties)
-    #         # response['properties with contracts'] = properties_removed
-
-    #         # Start actual query to add new contracts
-            
-    #         with connect() as db:
-    #             for i in range(len(properties)):
-    #                 print("loop", i)
-    #                 newContract = {}
-    #                 print(db.call('new_contract_uid'))
-    #                 newRequestID = db.call('new_contract_uid')['result'][0]['new_id']
-    #                 newContract['contract_uid'] = newRequestID
-    #                 print("In /contracts - POST. new contract UID = ", newRequestID)
-    #                 newContract["contract_property_id"] = properties[i]
-    #                 for field in fields:
-    #                     if field in data:
-    #                         newContract[field] = data.get(field)
-    #                         # print(newContract[field])
-
-    #                 response = db.insert('contracts', newContract)
-    #                 response['contract_UID'] = newRequestID
-    #         response['properties with contracts 2'] = properties_removed
-    #     return response
     
     def post(self):
         print("In Contracts POST ")
@@ -194,32 +58,7 @@ class Contracts(Resource):
             print("Properties affected: ", properties)
 
 
-            #  # Check if the property already has any active renewals for the given business
-            # index_to_remove = []
-            # properties_removed = []
-            # for i in range(len(properties)):
-            #     print('in Contracts Loop', len(properties), i, properties[i])
-            #     with connect() as db:
-            #         response = db.execute("""
-            #                     SELECT * From space.contracts
-            #                     WHERE contract_property_id = \'""" + properties[i] + """\' AND contract_business_id = \'""" + business_id + """\'
-            #                     AND contract_status in ('NEW','SENT');
-            #                     """)
-            #         if len(response['result']) != 0:
-            #             index_to_remove.append(i)
-            #             properties_removed.append(properties[i])
-            # print("Response: ", response)
-            # print("Properties ", properties)
-            # print("Index to Remove: ", index_to_remove)
-            # print("Properties to be removed: ", properties_removed)
-
-             # removes any properties from the List that already have contracts
-            # for i in sorted(index_to_remove, reverse = True):
-            #     del properties[i]
-            # print("Actual list of properties to be added:" , properties)
-
-        
-            # for i in range(len(properties)):
+             # Check if the property already has any active renewals for the given business
             for property in properties:
                 print("In loop processing: ", property)
 
@@ -243,17 +82,6 @@ class Contracts(Resource):
                 payload["contract_property_id"] = property
                 response["contract"] = db.insert('contracts', payload)
                 print("Data inserted into space.contracts", response)
-
-                # Add Contract Info
-                # payload['contract_documents'] = '[]' if payload.get('contract_documents') in {None, '', 'null'} else payload.get('contract_documents', '[]')
-                # print("Add Contract Payload: ", payload)  
-
-                # payload["contract_uid"] = contract_uid 
-                # payload["contract_property_id"] = property
-                # response['Add Contract'] = db.insert('contracts', payload)
-                
-                # response['Contract Documents Added'] = payload.get('contract_documents', "None")
-                # print("\nNew Contract Added")
 
         return response
 
@@ -290,11 +118,6 @@ class Contracts(Resource):
             print("Checking Inputs: ", key, payload)
             response['contract_info'] = db.update('contracts', key, payload)
             # print("Response:" , response)
-
-        # Get all contracts between Owner - PM
-        # If no other contracts then end
-        # If new contract has a start date <= today then make it Active
-        # If new contract becomes active, make other contracts Inactive
         
         return response
 
--- queries.py
+++ queries.py
@@ -813,7 +813,7 @@ def ContractDetails(user_id):
                 SELECT -- *,
                     -- property_id, property_unit, property_address, property_city, property_state, property_zip, property_owner_id, po_owner_percent
                     p.*
-                    , owner_uid, owner_user_id, owner_first_name, owner_last_name, owner_phone_number, owner_email
+                    , owner_uid, owner_user_id, po_owner_percent, owner_first_name, owner_last_name, owner_phone_number, owner_email
                     -- , owner_address, owner_unit, owner_city, owner_state, owner_zip
                     , owner_photo_url
                     , contract_uid, contract_property_id, contract_business_id, contract_start_date, contract_end_date, contract_fees, contract_assigned_contacts, contract_documents, contract_name, contract_status, contract_renew_status, contract_early_end_date, contract_end_notice_period, contract_m2m
@@ -828,11 +828,40 @@ def ContractDetails(user_id):
                 -- WHERE b.business_uid = \'""" + user_id + """\';
                 WHERE {column} =  \'""" + user_id + """\'  
                     """
+    
+    queryPM = """
+            SELECT -- *,
+                -- property_id, property_unit, property_address, property_city, property_state, property_zip, property_owner_id, po_owner_percent
+                p.*
+                , o.*
+                , contract_uid, contract_property_id, contract_business_id, contract_start_date, contract_end_date, contract_fees, contract_assigned_contacts, contract_documents, contract_name, contract_status, contract_renew_status, contract_early_end_date, contract_end_notice_period, contract_m2m
+                , business_uid, business_user_id, business_type, business_name, business_phone_number, business_email, business_services_fees
+                -- , business_address, business_unit, business_city, business_state, business_zip, business_photo_url
+            FROM space.properties p
+            LEFT JOIN (SELECT property_id, JSON_ARRAYAGG(JSON_OBJECT
+                                ('owner_uid', owner_uid,
+                                'owner_user_id', owner_user_id,
+                                'po_owner_percent', po_owner_percent,
+                                'owner_first_name', owner_first_name,
+                                'owner_last_name', owner_last_name,
+                                'owner_phone_number', owner_phone_number,
+                                'owner_email', owner_email,
+                                'owner_photo_url', owner_photo_url
+                                )) AS owners
+                                FROM space.o_details
+                                GROUP BY property_id) as o ON o.property_id = p.property_uid 
+            LEFT JOIN space.b_details b ON o.property_id = b.contract_property_id
+            -- WHERE b.business_uid = '600-000003'
+            -- WHERE o.owner_uid = '110-000003'
+            -- WHERE o.owner_uid = \'""" + user_id + """\';
+            -- WHERE b.business_uid = \'""" + user_id + """\';
+            WHERE {column} =  \'""" + user_id + """\'  
+            """
 
     if user_id.startswith("110"):
         query = query.format(column='owner_uid')
     elif user_id.startswith("600"):
-        query = query.format(column='business_uid')
+        query = queryPM.format(column='business_uid')
     else:
         print("Invalid condition type")
         return None
